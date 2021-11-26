07c5b4bef7a39b51fd63469bb284f45d49158ffe
Shade Soul <aijanmoinokova@gmail.com>
2021-11-26 13:30:35 +0300

    start coding

--- src/components/MainPage/Main.jsx
+++ src/components/MainPage/Main.jsx
@@ -1,18 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col, Space, Button, Input } from "antd";
 import logo from "../../media/Rectangle 1.png";
 import { Typography } from "antd";
 import "./main.css";
 import { UserOutlined, LockOutlined, MailOutlined } from "@ant-design/icons";
 import { Checkbox } from "antd";
+import { LogForm, RegForm } from "./Forms";
 
 const { Title } = Typography;
 
 const Main = () => {
-  function onChange(e) {
-    console.log(`checked = ${e.target.checked}`);
-  }
-
+  const [log, setLog] = useState(false);
   return (
     <>
       <Row>
@@ -36,9 +34,17 @@ const Main = () => {
               Jamskills - это сервис для автоматизации оценки <br /> сотрудников
               и кандидатов!
             </Title>
-            <Title disabled level={5}>
-              Если Вы уже зарегистрированны, <br /> войдите в свой кабинет
-            </Title>
+
+            {log ? (
+              <Title disabled level={5}>
+                Если Вы уже зарегистрированны, <br /> войдите в свой кабинет
+              </Title>
+            ) : (
+              <Title disabled level={5}>
+                Если Вы еще не зарегистрированны, <br />
+                создайте свой кабинет
+              </Title>
+            )}
             <Button
               style={{
                 border: "1px solid white",
@@ -46,50 +52,13 @@ const Main = () => {
                 paddingRight: "60px",
               }}
               type="primary"
+              onClick={() => setLog(!log)}
             >
-              Войти
+              {log ? "Войти" : "Создать"}
             </Button>
           </Space>
         </Col>
-        <Col span={15}>
-          <div className="mainRight">
-            <Title style={{ color: "#1890FF" }} level={1}>
-              Регистрация
-            </Title>
-            <div className="user">
-              <UserOutlined className="icon" />
-              <div>
-                <Input className="inp1" placeholder="Фамилия" />
-                <Input placeholder="Имя" />
-                <Input placeholder="Отчество" />
-              </div>
-            </div>
-            <div className="mail">
-              <MailOutlined className="icon" />
-              <Input placeholder="example@mail.com" />
-            </div>
-            <div className="lock">
-              <LockOutlined className="icon" />
-              <div>
-                <input placeholder="Пароль" type="password" />
-                <input placeholder="Повторите пароль" type="password" />
-              </div>
-            </div>
-            <span className="check">
-              <Checkbox onChange={onChange}>
-                <span style={{ color: "black" }}> Принимаю</span>
-              </Checkbox>{" "}
-              Пользовательское соглашение и Политику конфиденциальности
-            </span>
-            <Button
-              style={{ width: "230px", margin: "0 auto" }}
-              type="primary"
-              size="middle"
-            >
-              Создать
-            </Button>
-          </div>
-        </Col>
+        <Col span={15}>{log ? <RegForm /> : <LogForm />}</Col>
       </Row>
     </>
   );
--- src/routes/Pages.jsx
+++ src/routes/Pages.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Home from "../components/HomePage/Home";
 import Main from "../components/MainPage/Main";
 
 const Pages = () => {
   return (
     <Routes>
       <Route exact path="/" element={<Main />} />
+      <Route exact path="/home" element={<Home />} />
     </Routes>
   );
 };
